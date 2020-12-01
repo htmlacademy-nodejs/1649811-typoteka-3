9add3fcf88599a34e109c3ce49e4d672347245d0
Alexandr Timofeev <tim31al@gmail.com>
2020-12-01 11:08:31 +0300

    module3 task1

--- src/express/index.js
+++ src/express/index.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const express = require(`express`);
+const chalk = require(`chalk`);
 const DEFAULT_PORT = 8080;
 
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
 const articlesRouter = require(`./routes/article-routes`);
 const categoriesRouter = require(`./routes/category-routes`);
+
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  console.error(chalk.red(err.stack));
+  res.status(500).send(`Internal server error.`);
+};
+
+// eslint-disable-next-line no-unused-vars
+const notFoundHandler = (req, res, next) => {
+  res.status(404).send(`Page not found.`);
+};
 
 const app = express();
 
@@ -15,5 +27,8 @@ app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
 app.use(`/categories`, categoriesRouter);
 
-app.listen(DEFAULT_PORT);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
+app.listen(DEFAULT_PORT, () =>
+  console.log(chalk.yellow(`Принимаю соединения на порт: ${DEFAULT_PORT}`)));
