a828eea83d7f41162a8669acfff6e902242936b3
Alexandr Timofeev <tim31al@gmail.com>
2020-12-07 10:52:50 +0300

    module3 task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,63 +1,41 @@
 'use strict';
 
-const http = require(`http`);
 const fs = require(`fs`).promises;
+const express = require(`express`);
 const chalk = require(`chalk`);
 const {HttpCode, DEFAULT_PORT} = require(`../../constants`);
-
 const FILE_DATA = `${__dirname}/../../../mock.json`;
 
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
+const router = new express.Router();
+const postsRoute = router.get(`/posts`, (req, res) => {
+  fs.readFile(FILE_DATA, `utf8`)
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+    .catch((err) => {
+      res.send([]);
+      console.log(err.message);
+    });
+});
 
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_DATA, `utf8`);
-        const data = JSON.parse(fileContent);
-        const listItems = data.map((item) => `<li>${item.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${listItems}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
+const app = express();
+app.use(express.json());
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>Typoteka</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-  res.end(template);
-};
+app.use(postsRoute);
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).send(`Not found`);
+});
+app.use((err, _req, _res, _next) => {
+  console.error(err.stack);
+});
 
 module.exports = {
   name: `--server`,
   run: async (arg) => {
     const port = Number.parseInt(arg, 10) || DEFAULT_PORT;
-    const httpServer = http.createServer(onClientConnect);
 
-    httpServer
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(chalk.red(`Ошибка при создании сервера`, err));
-        }
-
-        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      });
-  },
+    app.listen(port, () => {
+      console.log(chalk.yellow(`Server listen ${port} port`));
+    });
+  }
 };
