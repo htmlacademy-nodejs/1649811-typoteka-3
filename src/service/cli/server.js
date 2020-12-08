51cea6a5f64f8576fdd060401201c1f0baff8916
Vohmyanin Sergey Vasilevich <delagen@gmail.com>
2020-12-08 12:15:24 +0300

    async await in route

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,15 +8,15 @@ const FILE_DATA = `${__dirname}/../../../mock.json`;
 
 const router = new express.Router();
 const postsRoute = router.get(`/posts`, async (req, res) => {
-  fs.readFile(FILE_DATA, `utf8`)
-    .then((data) => {
+    try {
+      const data = await fs.readFile(FILE_DATA, `utf8`)
       res.send(JSON.parse(data));
-    })
-    .catch((err) => {
+    } catch (err) {
       res.send([]);
       console.log(err.message);
-    });
-});
+    }
+  }
+);
 
 const app = express();
 app.use(express.json());
