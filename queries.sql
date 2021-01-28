da0f7a4a03e11d91a855f4809b48819d580c35ae
Alexandr Timofeev <tim31al@gmail.com>
2021-01-28 10:51:48 +0300

    В queries.sql изменены названия столбцов

--- queries.sql
+++ queries.sql
@@ -8,15 +8,15 @@ SELECT * FROM categories;
 -- (идентификатор, наименование категории)
 --
 SELECT c.* FROM categories c
-    LEFT JOIN articles_categories ac ON c.id = ac.article_id
+    LEFT JOIN articles_categories ac ON c.id = ac."articleId"
 GROUP BY c.id;
 
 --
 -- Получить список категорий с количеством публикаций
 -- (идентификатор, наименование категории, количество публикаций в категории)
 --
-SELECT c.*, count(ac.article_id) as count_articles FROM categories c
-    LEFT JOIN articles_categories ac ON c.id = ac.article_id
+SELECT c.*, count(ac."articleId") as count_articles FROM categories c
+    LEFT JOIN articles_categories ac ON c.id = ac."articleId"
 GROUP BY c.id;
 
 --
@@ -26,15 +26,15 @@ GROUP BY c.id;
 --  количество комментариев, наименование категорий).
 -- Сначала свежие публикации
 --
-SELECT a.id, a.title, a.announce, a.created_at,
+SELECT a.id, a.title, a.announce, a."createdAt",
        u.firstname, u.lastname, u.email,
-       (SELECT count(c.id) FROM comments c WHERE c.article_id = a.id) as count_comments,
+       (SELECT count(c.id) FROM comments c WHERE c."articleId" = a.id) as count_comments,
        (SELECT string_agg(ct.title, ', ') FROM categories ct
-                                                   INNER JOIN articles_categories ac ON ct.id = ac.category_id AND ac.article_id = a.id) as categories
+                                                   INNER JOIN articles_categories ac ON ct.id = ac."categoryId" AND ac."articleId" = a.id) as categories
 FROM articles a
-         LEFT JOIN users u ON a.user_id = u.id
-GROUP BY a.id, u.id, a.created_at
-ORDER BY a.created_at DESC;
+         LEFT JOIN users u ON a."userId" = u.id
+GROUP BY a.id, u.id, a."createdAt"
+ORDER BY a."createdAt" DESC;
 
 --
 -- Полная информация определённой публикации
@@ -42,23 +42,23 @@ ORDER BY a.created_at DESC;
 -- имя и фамилия автора, контактный email,
 -- количество комментариев, наименование категорий)
 --
-SELECT a.id, a.title, a.announce, a.full_text, a.created_at, a.picture,
+SELECT a.id, a.title, a.announce, a."fullText", a."createdAt", a.picture,
        u.firstname, u.lastname, u.email,
-       (SELECT count(c.id) FROM comments c WHERE c.article_id = a.id) as count_comments,
+       (SELECT count(c.id) FROM comments c WHERE c."articleId" = a.id) as count_comments,
        (SELECT string_agg(ct.title, ', ') FROM categories ct
-                                                   JOIN articles_categories ac ON ct.id = ac.category_id AND ac.article_id = a.id) as categories
+                                                   JOIN articles_categories ac ON ct.id = ac."categoryId" AND ac."articleId" = a.id) as categories
 FROM articles a
-         LEFT JOIN users u ON u.id = a.user_id
+         LEFT JOIN users u ON u.id = a."userId"
 WHERE a.id = 2;
 
 --
 -- Список из 5 свежих комментариев
 -- (идентификатор комментария, идентификатор публикации, имя и фамилия автора, текст комментария)
 --
-SELECT c.id, c.article_id, u.firstname, u.lastname, c.text
+SELECT c.id, c."articleId", u.firstname, u.lastname, c.text
 FROM comments c
-         LEFT JOIN users u ON c.user_id = u.id
-ORDER BY c.created_at DESC
+         LEFT JOIN users u ON c."userId" = u.id
+ORDER BY c."createdAt" DESC
 LIMIT 5;
 
 --
@@ -66,11 +66,11 @@ LIMIT 5;
 -- (идентификатор комментария, идентификатор публикации, имя и фамилия автора, текст комментария).
 -- Сначала новые комментарии
 --
-SELECT c.id, c.article_id, u.firstname, u.lastname, c.text
+SELECT c.id, c."articleId", u.firstname, u.lastname, c.text
 FROM comments c
-         LEFT JOIN users u ON c.user_id = u.id
-WHERE c.article_id = 2
-ORDER BY c.created_at DESC;
+         LEFT JOIN users u ON c."userId" = u.id
+WHERE c."articleId" = 2
+ORDER BY c."createdAt" DESC;
 
 --
 -- Обновить заголовок определённой публикации на «Как я встретил Новый год»;
