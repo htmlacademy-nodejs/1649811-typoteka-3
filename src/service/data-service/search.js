8cacf40de0ad3e1da6fc20fdb80ed2d7ab1a147f
Alexandr Timofeev <tim31al@gmail.com>
2021-04-16 15:36:30 +0300

    search

--- src/service/data-service/search.js
+++ src/service/data-service/search.js
@@ -6,19 +6,21 @@ const Alias = require(`../model/alias`);
 class SearchService {
   constructor(sequelize) {
     this._Article = sequelize.models.Article;
+    this._sequelize = sequelize;
   }
 
   async findAll(searchText) {
-
     return await this._Article.findAll({
-      where: {
-        title: {
-          [Op.like]: `%${searchText}%`,
-        },
-      },
+      where: this._sequelize
+        .where(
+            this._sequelize.fn(`lower`, this._sequelize.col(`Article.title`)),
+            {
+              [Op.like]: `%${searchText.toLowerCase()}%`,
+            },
+        ),
       include: Alias.CATEGORIES,
+      order: [[`createdAt`, `DESC`]],
     });
-
   }
 }
 
